6aae3d2ef89aaf1dd7e7b498d605a47a1a51cb09
matth <matth@DESKTOP-J2AM31I>
2018-11-30 11:35:22 +0100

    try

--- src/acceleration_model.py
+++ src/acceleration_model.py
@@ -4,6 +4,13 @@ Created on 30 nov. 2018
 @author: matth
 '''
 
+import geometry
+import traffic
+
+def vitesse(p1, p2):
+    velocity = geometry.Vector(p1, p2)
+    
+
 def normalisation_vitesse(fichier):
     with open(fichier):
         
