4584045c32e4ced2096366a6b568d7616e80cc0b
matth <matth@10.1.125.72>
2018-12-03 13:51:54 +0100

    Création fonction vitesse

--- src/acceleration_model.py
+++ src/acceleration_model.py
@@ -10,10 +10,12 @@ import traffic
 def vitesse(p1, p2):
     """Défini la vitesse avec un pas de temps de 5s et de 2 points"""
     velocity = geometry.Vector(p1, p2)
-    
+    return velocity.prod_linear(1/5)
 
 def normalisation_vitesse(fichier):
-    with open(fichier):
+    with open(fichier) as flight:
+        for pos in flight :
+            
         
     
     
