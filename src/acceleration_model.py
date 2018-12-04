711ce5f10c83e0ef38c9a4f9911cbd9fe7734c3b
matth <matth@10.1.125.72>
2018-12-04 11:53:33 +0100

    Ajout de la fonction nextSpeedEGTS

--- src/acceleration_model.py
+++ src/acceleration_model.py
@@ -6,6 +6,23 @@ Created on 30 nov. 2018
 
 import geometry
 import traffic
+import math
+from numpy import roll
+
+TIRERADIUS = 0.56 #en mètre
+MAXEGTSTORQUE = 16000 #N.m
+EGTSPOWER = 49000 #VA
+BREAKAWAYRESISTANCE = 0.01 #daN/kg
+ROLLINGRESISTANCE = 0.007 #daN/kg
+AEROCOEF = 1.032
+TIMESTEP = 4.1 #s
+MASS_A319_DEP = 63000 #kg
+MASS_A319_ARR = 57000 #kg
+MASS_A320_DEP = 69000 #kg
+MASS_A320_ARR = 62000 #kg
+MASS_A321_DEP = 81000 #kg
+MASS_A321_ARR = 73000 #kg
+
 
 def vitesse(p1, p2):
     """Défini la vitesse avec un pas de temps de 5s et de 2 points"""
@@ -24,9 +41,24 @@ def normalisation_vitesse(fichier):
                 velocity[i].append(vitesse(point, point+1).unit())
         return velocity
     
-def modele_acceleration(fichier_pente,fichier):
+#def modele_acceleration(fichier_pente,fichier):
     """ recréer un nouveau fichier lfpg_flight.txt qui prend en compte la pente du terrain"""
     
+    
+def nextspeedegts(mass, slope, speed):
+    """calcul la prochaine vitesse pour un avion EGTS"""
+    slope_torque = - mass * 9.81 * math.sin(math.atan(slope/100)) * TIRERADIUS
+    if speed < 1:
+        res_torque = - mass * BREAKAWAYRESISTANCE * 10 * TIRERADIUS
+        egts_torque = MAXEGTSTORQUE
+    else:
+        res_torque = - mass * ROLLINGRESISTANCE * 10 * TIRERADIUS
+        egts_torque = min(MAXEGTSTORQUE,EGTSPOWER/(speed/TIRERADIUS))
+    aero_torque = AEROCOEF * speed**2
+    torque = egts_torque + slope_torque + res_torque + aero_torque
+    acc = max(0, torque/TIRERADIUS/mass)
+    return(speed + TIMESTEP * acc)
+        
 
                 
                 
